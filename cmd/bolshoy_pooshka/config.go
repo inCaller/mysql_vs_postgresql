2a0709d87dfe0ccf44bfa5c30bd810508c48887b
Alex Chistyakov <alexclear@gmail.com>
2016-05-28 19:14:13 +0300

    Rename Name to StageName

--- cmd/bolshoy_pooshka/config.go
+++ cmd/bolshoy_pooshka/config.go
@@ -7,7 +7,7 @@ type Config struct {
 }
 
 type Stage struct {
-	Name        string        // used as a part of metric name
+	StageName   string        // used as a part of metric name
 	RPS         float32       // 0 - infinity
 	Concurrency int           // How many repeatable requests must be run in parallel, 0 is 1
 	Duration    time.Duration /*
