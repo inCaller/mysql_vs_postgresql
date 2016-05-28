05de63cf03fc35b69223f3b80778fa23810cb60f
Daniel Podolsky <onokonem@gmail.com>
2016-05-28 18:36:22 +0300

    2 new fields added to config

--- cmd/bolshoy_pooshka/config.go
+++ cmd/bolshoy_pooshka/config.go
@@ -7,9 +7,10 @@ type Config struct {
 }
 
 type Stage struct {
-	Name     string        // used as a part of metric name
-	RPS      float32       // 0 - infinity
-	Duration time.Duration /*
+	Name        string        // used as a part of metric name
+	RPS         float32       // 0 - infinity
+	Concurrency int           // How many repeatable requests must be run in parallel, 0 is 1
+	Duration    time.Duration /*
 		0 - end as soon as all the RunOnce queries done
 		duration - obvious
 		set a huge duration to run until interrupted
@@ -22,5 +23,6 @@ type Stage struct {
 type Request struct {
 	Name        string  // used as a part of metric name
 	Query       string  // SQL itself
+	Update      bool    // This query is DB update
 	Probability float32 // 0 - never, 1 - each time, ignored for RunOnce
 }
