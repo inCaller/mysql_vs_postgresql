c1e684420f78c7bbf574f61a6001992dc7b0fb48
Daniel Podolsky <onokonem@gmail.com>
2016-05-28 16:41:08 +0300

    config struct

---
+++ cmd/bolshoy_pooshka/config.go
@@ -0,0 +1,24 @@
+package main
+
+import "time"
+
+type Config struct {
+	Stages []Stage
+}
+
+type Stage struct {
+	Name     string
+	RPS      float32       // 0 - infinity
+	Duration time.Duration /*
+		0 - end as soon as all the RunOnce queries done
+		duration - obvious
+		set a huge duration to run until interrupted
+	*/
+	RunOnce []Request // executed one by one
+	Repeat  []Request // executed in parallel according to their probability
+}
+
+type Request struct {
+	Query       string  // SQL itself
+	Probability float32 // 0 - never, 1 - each time, ignored for RunOnce
+}
