6e73185157fbad7b1ae97e176d696b9534a149f1
Daniel Podolsky <onokonem@gmail.com>
2016-06-01 16:55:02 +0300

    string instead of byte array

--- cmd/bolshy_pooshka/book1.go
+++ cmd/bolshy_pooshka/book1.go
@@ -4,10 +4,10 @@ import "math/rand"
 
 //go:generate go-bindata -pkg $GOPACKAGE -o book1txt.go book1.txt
 
-var txt = MustAsset("book1.txt")
+var txt = string(MustAsset("book1.txt"))
 
 func GetRandText(maxLen int) string {
 	txtLen := rand.Intn(maxLen/10*9) + maxLen/10
 	txtPos := rand.Intn(len(txt) - txtLen)
-	return string(txt[txtPos : txtPos+txtLen])
+	return txt[txtPos : txtPos+txtLen]
 }
