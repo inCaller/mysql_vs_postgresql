862db9a1969cbab55b28bf79ec4f630cb6b334cc
Daniel Podolsky <onokonem@gmail.com>
2016-06-01 14:52:52 +0300

    some source for random text added

--- cmd/bolshy_pooshka/book1.go
+++ cmd/bolshy_pooshka/book1.go
@@ -1,3 +1,11 @@
 package main
 
+import "math/rand"
+
 //go:generate go-bindata -pkg $GOPACKAGE -o book1txt.go book1.txt
+
+func GetRandText(maxLen int) string {
+	txtLen := rand.Intn(maxLen)
+	txtPos := rand.Intn(len(_book1Txt) - txtLen)
+	return string(_book1Txt[txtPos : txtPos+txtLen])
+}
